7a4b41f5e6765036bb723cfa37f8b71f15127194
Katherine Zorina <ekaterina.zorina.eneca@gmail.com>
2025-04-14 11:34:31 +0300

    Добавлено управление иконками сайта в метаданные и в заголовок документа, включая различные размеры иконок для улучшения отображения на устройствах.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Project Dynamic',
   description: 'Просмотр динамики прогресса проекта и секций',
   generator: 'v0.dev',
+  icons: {
+    icon: [
+      { url: '/faviconBig.ico' },
+      { url: '/favicon-32x32.ico', sizes: '32x32' },
+      { url: '/icon.png', type: 'image/png' }
+    ],
+  }
 }
 
 import { RootClientLayout } from './root-client-layout'
@@ -16,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/faviconBig.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32x32.ico" sizes="32x32" />
+        <link rel="shortcut icon" href="/favicon-32x32.ico" type="image/x-icon" />
+        <link rel="icon" href="/icon.png" type="image/png" />
+      </head>
       <body>
         <RootClientLayout>
           {children}
